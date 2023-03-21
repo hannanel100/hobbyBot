37dd23ef8fb42fbed86d58f023ef9757e081279f
hannanel100 <hannanel100@gmail.com>
2023-03-21 15:38:31 +0200

    uncommenting questions

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -49,107 +49,107 @@ const questions: Question[] = [
     options: ["Running", "Swimming", "Yoga", "Weightlifting"],
     answer: "",
   },
-  // {
-  //   question: "Do you prefer indoor or outdoor activities?",
-  //   options: ["Indoor", "Outdoor", "Both", "Depends on the activity"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "How important is socializing to you?",
-  //   options: [
-  //     "Very important",
-  //     "Somewhat important",
-  //     "Not important",
-  //     "Depends on the activity",
-  //   ],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy reading or watching movies/TV shows?",
-  //   options: ["Reading", "Watching", "Both", "Neither"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "What kind of music do you enjoy?",
-  //   options: ["Rock", "Pop", "Hip hop", "Classical"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy working with your hands?",
-  //   options: ["Yes", "No", "Depends on the activity", "I'm not sure"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy cooking or baking?",
-  //   options: ["Cooking", "Baking", "Both", "Neither"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy playing games?",
-  //   options: ["Board games", "Video games", "Card games", "Sports games"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "What kind of art do you enjoy?",
-  //   options: ["Drawing", "Painting", "Sculpture", "None"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy learning about history or science?",
-  //   options: ["History", "Science", "Both", "Neither"],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy traveling?",
-  //   options: [
-  //     "Yes, I love it!",
-  //     "I like it, but don't do it often",
-  //     "I don't really care for it",
-  //     "I haven't done it much",
-  //   ],
-  //   answer: "",
-  // },
+  {
+    question: "Do you prefer indoor or outdoor activities?",
+    options: ["Indoor", "Outdoor", "Both", "Depends on the activity"],
+    answer: "",
+  },
+  {
+    question: "How important is socializing to you?",
+    options: [
+      "Very important",
+      "Somewhat important",
+      "Not important",
+      "Depends on the activity",
+    ],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy reading or watching movies/TV shows?",
+    options: ["Reading", "Watching", "Both", "Neither"],
+    answer: "",
+  },
+  {
+    question: "What kind of music do you enjoy?",
+    options: ["Rock", "Pop", "Hip hop", "Classical"],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy working with your hands?",
+    options: ["Yes", "No", "Depends on the activity", "I'm not sure"],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy cooking or baking?",
+    options: ["Cooking", "Baking", "Both", "Neither"],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy playing games?",
+    options: ["Board games", "Video games", "Card games", "Sports games"],
+    answer: "",
+  },
+  {
+    question: "What kind of art do you enjoy?",
+    options: ["Drawing", "Painting", "Sculpture", "None"],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy learning about history or science?",
+    options: ["History", "Science", "Both", "Neither"],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy traveling?",
+    options: [
+      "Yes, I love it!",
+      "I like it, but don't do it often",
+      "I don't really care for it",
+      "I haven't done it much",
+    ],
+    answer: "",
+  },
 
-  // {
-  //   question: "Do you enjoy gardening or caring for plants?",
-  //   options: [
-  //     "Yes, I love it!",
-  //     "I do it occasionally",
-  //     "I don't really do it",
-  //     "I haven't done it much",
-  //   ],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy attending concerts or live events?",
-  //   options: [
-  //     "Yes, I love it!",
-  //     "I enjoy it occasionally",
-  //     "I don't really care for it",
-  //     "I haven't done it much",
-  //   ],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy working on DIY projects or home improvement tasks?",
-  //   options: [
-  //     "Yes, I love it!",
-  //     "I do it occasionally",
-  //     "I don't really do it",
-  //     "I haven't done it much",
-  //   ],
-  //   answer: "",
-  // },
-  // {
-  //   question: "Do you enjoy practicing or playing musical instruments?",
-  //   options: [
-  //     "Yes, I love it!",
-  //     "I do it occasionally",
-  //     "I don't really do it",
-  //     "I haven't done it much",
-  //   ],
-  //   answer: "",
-  // },
+  {
+    question: "Do you enjoy gardening or caring for plants?",
+    options: [
+      "Yes, I love it!",
+      "I do it occasionally",
+      "I don't really do it",
+      "I haven't done it much",
+    ],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy attending concerts or live events?",
+    options: [
+      "Yes, I love it!",
+      "I enjoy it occasionally",
+      "I don't really care for it",
+      "I haven't done it much",
+    ],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy working on DIY projects or home improvement tasks?",
+    options: [
+      "Yes, I love it!",
+      "I do it occasionally",
+      "I don't really do it",
+      "I haven't done it much",
+    ],
+    answer: "",
+  },
+  {
+    question: "Do you enjoy practicing or playing musical instruments?",
+    options: [
+      "Yes, I love it!",
+      "I do it occasionally",
+      "I don't really do it",
+      "I haven't done it much",
+    ],
+    answer: "",
+  },
   {
     question: "How many hobby ideas do you want to see?",
     options: ["1", "2", "3", "4"],
